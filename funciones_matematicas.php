14b6630b22ef2f2073487777de3c3fcabbc7c52e
MatiasDeMoliner2026 <demo.matias14@gmail.com>
2026-09-10 11:58:57 -0300

    Add files via upload

---
+++ funciones_matematicas.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 5 a la 5*/
+echo pow(5, 5). "<br>";
+/*LA raiz cuadrada de*/
+echo sqrt(100)."<br>";
+/* Num random entre esos*/
+echo rand(1, 55). "<br>";
+/*muestra a pi*/
+echo pi(). "<br>";
+/*redondea pa abajo*/
+echo floor(6.9). "<br>";
+/*redondea pa arriba*/
+echo ceil(4.9). "<br>";
+/*redondea <depende*/
+echo round(4.1). "<br>";
+
+
---
+++ number_format.php
@@ -0,0 +1,9 @@
+<?php
+
+$cant_1=12372.77;
+$cant_2=1931.81;
+
+//$number_format=(cantidad. decimales. sep_decimal. sep_militar);
+
+$cant_2=number_format($cant_2,0,".", ".");
+echo $cant_2;
